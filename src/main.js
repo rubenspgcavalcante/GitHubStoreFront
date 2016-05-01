36942fae07ab1a55334cf7804fdae71ec0ee2ffd
Rubens Pinheiro <rubenspgcavalcante@gmail.com>
2016-05-01 16:46:39 -0300

    Missing deps

--- src/main.js
+++ src/main.js
@@ -2,17 +2,17 @@ require.config({
     baseUrl: '/',
 
     paths: {
-        is: 'vendor/is_js/is',
-        EventEmitter: 'vendor/eventEmitter/EventEmitter',
-        jquery: 'vendor/jquery/dist/jquery',
-        underscore: 'vendor/underscore/underscore',
-        bootstrap: 'vendor/bootstrap/dist/js/bootstrap',
-        react: 'vendor/react/react-with-addons',
-        reactDom: 'vendor/react/react-dom',
-        flux: 'vendor/flux/dist/Flux',
-        text: 'vendor/requirejs-text/text',
-        jsx: 'vendor/jsx-requirejs-plugin/js/jsx',
-        JSXTransformer: 'vendor/jsx-requirejs-plugin/js/JSXTransformer'
+        is: './vendor/is_js/is',
+        EventEmitter: './vendor/eventEmitter/EventEmitter',
+        jquery: './vendor/jquery/dist/jquery',
+        underscore: './vendor/underscore/underscore',
+        bootstrap: './vendor/bootstrap/dist/js/bootstrap',
+        react: './vendor/react/react-with-addons',
+        reactDom: './vendor/react/react-dom',
+        flux: './vendor/flux/dist/Flux',
+        text: './vendor/requirejs-text/text',
+        jsx: './vendor/jsx-requirejs-plugin/js/jsx',
+        JSXTransformer: './vendor/jsx-requirejs-plugin/js/JSXTransformer'
     },
     jsx: {
         fileExtension: '.jsx'
@@ -22,5 +22,5 @@ require.config({
             exports: 'componentHandler'
         }
     },
-    deps: ['app/app.js']
+    deps: ['./app/app.js']
 });
